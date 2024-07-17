9b2e1481f1a0e036d4db97c5bc81c5b01cb08db8
coldcode006 <66368695+coldcode006@users.noreply.github.com>
2024-07-17 21:36:12 +0800

    Update SAT.py

--- SAT.py
+++ SAT.py
@@ -46,11 +46,8 @@ class sat_layer3d(nn.Module):
         self.avg_pool1 = nn.AdaptiveAvgPool3d((self.d, 1, 1))
         self.avg_pool2 = nn.AdaptiveAvgPool3d((1, self.h, self.w))
         self.cweight = Parameter(torch.zeros(1, channel // (2 * groups), self.d, 1, 1))
-        self.cbias = Parameter(torch.ones(1, channel // (2 * groups), self.d, 1, 1))
-        # self.sweight = Parameter(torch.zeros(1, channel // (2 * groups), 1,1, 1))
-        # self.sbias = Parameter(torch.ones(1, channel // (2 * groups),1, 1, 1))
+        self.cbias = Parameter(torch.ones(1, channel // (2 * groups), self.d, 1, 1)
         self.sigmoid = nn.Sigmoid()
-        # self.gn = nn.GroupNorm(channel // (2 * groups), channel // (2 * groups))
         self.conv1 = nn.Conv3d(in_channels=2, out_channels=1, kernel_size=(3, 3, 3), stride=1, padding=1, bias=False)
         self.fc = nn.Sequential(
             nn.Linear(channel, channel // reduction, bias=False),
@@ -75,7 +72,6 @@ class sat_layer3d(nn.Module):
 
         x = x.reshape(b * self.groups, -1, d, h, w)
         x_0, x_1 = x.chunk(2, dim=1)
-        #         b, c,_,d, h, w = x_0.shape
 
         # avg_pool1 = nn.AdaptiveAvgPool3d((d,1,1))
         # avg_pool2 = nn.AdaptiveAvgPool3d((1,h,w))
@@ -83,21 +79,13 @@ class sat_layer3d(nn.Module):
         xn = self.avg_pool1(x_0)
         xn = self.cweight * xn + self.cbias
         xn = x_0 * self.sigmoid(xn)
-        # max_out=self.max_pool(x_0).view([b,c])
-        # # max_out = self.fc(self.max_pool(x_0))
-        # # avg_out = self.fc(self.avg_pool(x_0))
-        # avg_out=self.avg_pool(x_0).view([b,c])
-        # max_fc_out=self.fc(max_out)
-        # avg_fc_out=self.fc(avg_out)
-        # channel_out = self.sigmoid(max_fc_out + avg_fc_out)
-        # xn = channel_out * x_0
+
 
         # spatial attention
         xs_1 = torch.mean(x_1, dim=1, keepdim=True)
         xs_2 = torch.max(x_1, dim=1, keepdim=True)[0]
         xs = torch.cat([xs_1, xs_2], dim=1)
 
-        # xs=self.avg_pool2 (xs)
         xs = self.conv1(xs)
 
         xs = self.sigmoid(xs)
@@ -108,3 +96,159 @@ class sat_layer3d(nn.Module):
 
         out = self.channel_shuffle(out, 2)
         return out
+class ResNeXt(nn.Module):
+
+    def __init__(self,
+                 block,
+                 layers,
+                 block_inplanes,
+                 input_size,
+                 n_input_channels=3,
+                 conv1_t_size=7,
+                 conv1_t_stride=1,
+                 no_max_pool=False,
+                 shortcut_type='B',
+                 cardinality=32,
+                 widen_factor=1.0,
+                 n_classes=101):
+        #block = partialclass(block, cardinality=cardinality)
+        # #super().__init__(block, layers, block_inplanes, n_input_channels,
+        #                  conv1_t_size, conv1_t_stride, no_max_pool,
+        #                  shortcut_type, n_classes)
+        super().__init__()
+        block_inplanes = [int(x * widen_factor) for x in block_inplanes]
+
+        self.in_planes = block_inplanes[0]
+        self.no_max_pool = no_max_pool
+        self.conv1 = nn.Conv3d(n_input_channels,
+                               out_channels=64 ,
+                               kernel_size=(conv1_t_size, 7, 7),
+                               stride=(conv1_t_stride, 2, 2),
+                               padding=(conv1_t_size // 2, 3, 3),
+                               bias=False)
+        self.bn1 = nn.BatchNorm3d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+        
+        self.layer1 = self._make_layer(block, block_inplanes[0],
+                                       layers[0],
+                                       cardinality,
+                                       shortcut_type)
+        self.layer2 = self._make_layer(block,
+                                       block_inplanes[1],
+                                       layers[1],
+                                       cardinality,
+                                       shortcut_type,
+                                       stride=2)
+        self.layer3 = self._make_layer(block,
+                                       block_inplanes[2],
+                                       layers[2],
+                                       cardinality,
+                                       shortcut_type,
+                                       stride=2)
+        self.layer4 = self._make_layer(block,
+                                       block_inplanes[3],
+                                       layers[3],
+                                       cardinality,
+                                       shortcut_type,
+                                       stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool3d((1, 1, 1))
+        self.fc = nn.Linear(cardinality * 32 * block.expansion, n_classes)
+        self.sa1 = sa_layer3d(256,input_size[0])
+        self.sa2 = sa_layer3d(512,input_size[1])
+        self.sa3 = sa_layer3d(1024,input_size[2])
+        self.sa4 = sa_layer3d(2048,input_size[3])
+        
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                nn.init.kaiming_normal_(m.weight,
+                                        mode='fan_out',
+                                        nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm3d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+                
+    def _downsample_basic_block(self, x, planes, stride):
+        out = F.avg_pool3d(x, kernel_size=1, stride=stride)
+        zero_pads = torch.zeros(out.size(0), planes - out.size(1), out.size(2),
+                                out.size(3), out.size(4))
+        if isinstance(out.data, torch.cuda.FloatTensor):
+            zero_pads = zero_pads.cuda()
+
+        out = torch.cat([out.data, zero_pads], dim=1)
+
+        return out
+    
+    def _make_layer(self, block, planes, blocks,cardinality,shortcut_type, stride=1):
+        downsample = None
+        if stride != 1 or self.in_planes != planes * block.expansion:
+            if shortcut_type == 'A':
+                downsample = partial(self._downsample_basic_block,
+                                     planes=planes * block.expansion,
+                                     stride=stride)
+            else:
+                downsample = nn.Sequential(
+                    conv1x1x1(self.in_planes, planes * block.expansion, stride),
+                    nn.BatchNorm3d(planes * block.expansion))
+
+        layers = []
+        layers.append(
+            block(in_planes=self.in_planes,
+                  planes=planes,
+                  stride=stride,
+                  cardinality=cardinality,
+                  downsample=downsample))
+        self.in_planes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(in_planes=self.in_planes, planes=planes,cardinality=cardinality))
+
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        if not self.no_max_pool:
+            x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.sa1(x)
+        x = self.layer2(x)
+        x = self.sa2(x)
+        x = self.layer3(x)
+        x = self.sa3(x)
+        x = self.layer4(x)
+        x = self.sa4(x)
+
+        x = self.avgpool(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+    
+def generate_model_resnext(model_depth, **kwargs):
+    assert model_depth in [50, 101, 152, 200]
+    
+    x1=torch.randn([4,256,8,28,28])
+    x2=torch.randn([4,256,4,14,14])
+    x3=torch.randn([4,256,2,7,7])
+    x4=torch.randn([4,256,1,4,4])
+    input_size=[x1,x2,x3,x4]
+    if model_depth == 50:
+        model = ResNeXt(ResNeXtBottleneck, [3, 4, 6, 3], get_inplanes(),input_size,
+                        **kwargs)
+    elif model_depth == 101:
+        model = ResNeXt(ResNeXtBottleneck, [3, 4, 23, 3], get_inplanes(),input_size,
+                        **kwargs)
+    elif model_depth == 152:
+        model = ResNeXt(ResNeXtBottleneck, [3, 8, 36, 3], get_inplanes(),input_size,
+                        **kwargs)
+    elif model_depth == 200:
+        model = ResNeXt(ResNeXtBottleneck, [3, 24, 36, 3], get_inplanes(),input_size,
+                        **kwargs)
+
+    return model
+
